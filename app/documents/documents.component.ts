c0197557071c66668a9ed6891ab6e16acf7dc0cc
MirTalpur <hskalee123@gmail.com>
2016-12-16 15:14:41 -0800

    Init have documents showing mock-data

---
+++ app/documents/documents.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Document } from './document';
+
+@Component({
+  moduleId: module.id,
+  selector: 'documents',
+  templateUrl: 'documents.component.html'
+})
+
+export class DocumentsComponent{
+  documents: Document[] = [
+    {
+      title: "My first Doc",
+      description: 'aldkfjs aljksdf',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    },
+    {
+      title: "My Second Doc",
+      description: 'aldkfjs aljksdf',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    },
+    {
+      title: "My last Doc",
+      description: 'aldkfjs aljksdf',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    }
+  ]
+}
