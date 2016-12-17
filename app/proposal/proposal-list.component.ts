3515bfd44a534d3e016ab0f62f220fef131fcb74
MirTalpur <hskalee123@gmail.com>
2016-12-16 16:07:47 -0800

    Added proposals to the view

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   moduleId: module.id,
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: 'proposal-list.component.html'
 })
 export class ProposalListComponent {
-  
+  proposalOne: Proposal = new Proposal(
+    15,'Abc company', 'http://portfolio.jordanhdgens.com', 
+    'Ruby on Rails', 
+    150,120, 15, 
+    'jordan@devcamp.com')
+  proposalTwo: Proposal = new Proposal(
+    99,'Abcxyz company', 'http://portfolio.jordanhdgens.com', 
+    'Ruby on Rails', 
+    150,120, 15, 
+    'jordan@devcamp.com')
+  proposalThree: Proposal = new Proposal(
+    300,'Dude company', 'http://portfolio.jordanhdgens.com', 
+    'Ruby on Rails', 
+    150,120, 15, 
+    'jordan@devcamp.com')
+
+  proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+  ]
 }
