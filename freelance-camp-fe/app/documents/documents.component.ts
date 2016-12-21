7c5b415e5f4f740d2c14500dbcd956e6f6ceed28
MirTalpur <hskalee123@gmail.com>
2016-12-21 12:38:38 -0800

    Added service in angular2 and changed the api for docs

--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -1,37 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
 
-export class DocumentsComponent{
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Documents Dashboard"
-  
-  documents: Document[] = [
-    {
-      title: "My first Doc",
-      description: 'aldkfjs aljksdf',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://www.policygenius.com/blog/library/uploads/2015/07/Freelancer-Liability-Featured.png'
-    },
-    {
-      title: "My Second Doc",
-      description: 'aldkfjs aljksdf',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://www.policygenius.com/blog/library/uploads/2015/07/Freelancer-Liability-Featured.png'
-    },
-    {
-      title: "My last Doc",
-      description: 'aldkfjs aljksdf',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://www.policygenius.com/blog/library/uploads/2015/07/Freelancer-Liability-Featured.png'
-    }
-  ]
+  documents: Document[] = [];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private documentService: DocumentService;
+  ){}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+         documents => this.documents = documents,
+         error => this.errorMessage = <any>error
+        );
+  }  
 }
